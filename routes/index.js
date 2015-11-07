fe8ca4270d8c18e902986028bf654a95a7af901f
Junaid Farooq <ijunaidfarooq@gmail.com>
2015-11-07 07:52:28 +0500

    Deleted Response object and pas the DB returend variable directly.

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,9 @@ var mongojs = require('mongojs');
 exports.books = {};
 
 exports.books.all =  function(req,res){
-	db.books.find({},function(err,books){
+	db.books.find(function(err,books){
 		if (err) return;
-		var response = {
-			books: books
-		};
-		res.json(response);
+		res.json(books);
 	});
 };
 
@@ -19,9 +16,6 @@ exports.books.one = function(req,res){
 	console.log(bookId);
 	db.books.findOne({ _id: bookId}, function(err,book){
 		if (err) return;
-		var response = {
-			book : book
-		};
-		res.json(response);
+		res.json(book);
 	});
 };
